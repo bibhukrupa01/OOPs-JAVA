60d8dae34d9659e207ef48e611240ca793d8e5c8
Bibhu krupa <bibhukrupa5@gmail.com>
2025-09-19 11:21:26 +0530

    Create staticblock.java

---
+++ staticblock.java
@@ -0,0 +1,8 @@
+class Main{
+    static{
+        System.out.println("Static block initialised");
+    }
+    public static void main(String[] args){
+        System.out.println("from main");
+    }
+}
